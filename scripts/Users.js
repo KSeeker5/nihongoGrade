e97d0593ed165f3e6671253d6761bc0bc05676ad
KSeeker5 <alexknipper@comcast.net>
2018-09-27 22:22:42 -0400

    Add files via upload

--- scripts/Users.js
+++ scripts/Users.js
@@ -1,7 +1,9 @@
 var usersArr = [
-	'U2FsdGVkX1+kxaGiTEEzZPhxm1V9uVG74s6gc7TRKG4='
+	'U2FsdGVkX1+kxaGiTEEzZPhxm1V9uVG74s6gc7TRKG4=',
+	'U2FsdGVkX1+o207SNXLPIFtciarcVA/XOgkvzNANyvU='
 ]
 
 var idArr = [
-	'U2FsdGVkX1/NSUMyjMFPH2wYnwVJXnDk8JtplCpQ08I='
+	'U2FsdGVkX1/NSUMyjMFPH2wYnwVJXnDk8JtplCpQ08I=',
+	'U2FsdGVkX1/54w8ZD3Hhb8QKh4VsYnHp73CuZ/3XJJU='
 ]
