aef2fd4382c3bf4f4d2dd5549b93374d6cb36e74
KSeeker5 <alexknipper@comcast.net>
2018-09-30 17:06:37 -0400

    Add files via upload

---
+++ scripts/assignments/Homework 1.js
@@ -0,0 +1,27 @@
+var questionsArr = [
+	"What is the hiragana for the romaji 'a'?",
+	"What is the hiragana for the romaji 'u'?",
+	"What is the hiragana for the romaji 'i'?",
+	"What is the hiragana for the romaji 'e'?",
+	"What is the hiragana for the romaji 'o'?",
+	"What is the romaji for the hiragana 'あ'?",
+	"What is the romaji for the hiragana 'う'?",
+	"What is the romaji for the hiragana 'い'?",
+	"What is the romaji for the hiragana 'え'?",
+	"What is the romaji for the hiragana 'お'?"
+]
+
+var answersArr = [
+	"あ",
+	"う",
+	"い",
+	"え",
+	"お",
+	"a",
+	"u",
+	"i",
+	"e",
+	"o"
+]
+
+var max_score = 10;
