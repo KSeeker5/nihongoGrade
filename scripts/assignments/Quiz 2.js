757f67919b545f2c7ec540cc16fd540ab729a0bd
KSeeker5 <alexknipper@comcast.net>
2018-09-30 23:07:15 -0400

    Add files via upload

---
+++ scripts/assignments/Quiz 2.js
@@ -0,0 +1,27 @@
+var questionsArr = [
+	"What is the hiragana for the romaji 'ka'?",
+	"What is the hiragana for the romaji 'ki'?",
+	"What is the hiragana for the romaji 'ke'?",
+	"What is the hiragana for the romaji 'ko'?",
+	"What is the hiragana for the romaji 'ku'?",
+	"What is the romaji for the hiragana 'か'?",
+	"What is the romaji for the hiragana 'き'?",
+	"What is the romaji for the hiragana 'け'?",
+	"What is the romaji for the hiragana 'こ'?",
+	"What is the romaji for the hiragana 'く'?"
+]
+
+var answersArr = [
+	"か",
+	"き",
+	"け",
+	"こ",
+	"く",
+	"ka",
+	"ki",
+	"ke",
+	"ko",
+	"ku"
+]
+
+var max_score = 10;
