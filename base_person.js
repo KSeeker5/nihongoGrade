b8182d568fe87f465e37f307556188640d2fc162
KSeeker5 <alexknipper@comcast.net>
2018-09-30 23:10:02 -0400

    Add files via upload

---
+++ base_person.js
@@ -0,0 +1,28 @@
+var name = "Name";
+
+var participationGrades = [
+	-1
+]
+
+var homeworkGrades = [
+	-1,
+	-1,
+	-1,
+	-1
+]
+
+var quizGrades = [
+	-1,
+	-1
+]
+
+var testGrades = [
+	-1
+]
+
+var examGrades = [
+	-1
+]
+
+var completedAssignments = [
+]
