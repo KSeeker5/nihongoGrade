d6a22d824aba4a7ec9f783682d90cff2d15bc74a
KSeeker5 <alexknipper@comcast.net>
2018-10-01 23:46:57 -0400

    Add files via upload

---
+++ scripts/assignments/Homework 3.js
@@ -0,0 +1,47 @@
+var questionsArr = [
+	"What is the hiragana for the romaji 'za'?",
+	"What is the hiragana for the romaji 'ze'?",
+	"What is the hiragana for the romaji 'zo'?",
+	"What is the hiragana for the romaji 'zu'?",
+	"What is the hiragana for the romaji 'ji'?",
+	"What is the romaji for the hiragana 'ざ'?",
+	"What is the romaji for the hiragana 'ぜ'?",
+	"What is the romaji for the hiragana 'ぞ'?",
+	"What is the romaji for the hiragana 'ず'?",
+	"What is the romaji for the hiragana 'じ'?",
+	"What is the hiragana for the romaji 'ta'?",
+	"What is the hiragana for the romaji 'te'?",
+	"What is the hiragana for the romaji 'to'?",
+	"What is the hiragana for the romaji 'chi'?",
+	"What is the hiragana for the romaji 'tsu'?",
+	"What is the romaji for the hiragana 'た'?",
+	"What is the romaji for the hiragana 'て'?",
+	"What is the romaji for the hiragana 'と'?",
+	"What is the romaji for the hiragana 'ち'?",
+	"What is the romaji for the hiragana 'つ'?"
+]
+
+var answersArr = [
+	"ざ",
+	"ぜ",
+	"ぞ",
+	"ず",
+	"じ",
+	"za",
+	"ze",
+	"zo",
+	"zu",
+	"ji",
+	"た",
+	"て",
+	"と",
+	"ち",
+	"つ",
+	"ta",
+	"te",
+	"to",
+	"chi",
+	"tsu"
+]
+
+var max_score = 20;
---
+++ scripts/assignments/Homework 4.js
@@ -0,0 +1,47 @@
+var questionsArr = [
+	"What is the hiragana for the romaji 'da'?",
+	"What is the hiragana for the romaji 'de'?",
+	"What is the hiragana for the romaji 'do'?",
+	"What is the hiragana for the romaji 'ji'?",
+	"What is the hiragana for the romaji 'zu'?",
+	"What is the romaji for the hiragana 'だ'?",
+	"What is the romaji for the hiragana 'で'?",
+	"What is the romaji for the hiragana 'ど'?",
+	"What is the romaji for the hiragana 'ぢ'?",
+	"What is the romaji for the hiragana 'づ'?",
+	"What is the hiragana for the romaji 'na'?",
+	"What is the hiragana for the romaji 'ni'?",
+	"What is the hiragana for the romaji 'ne'?",
+	"What is the hiragana for the romaji 'no'?",
+	"What is the hiragana for the romaji 'nu'?",
+	"What is the romaji for the hiragana 'な'?",
+	"What is the romaji for the hiragana 'に'?",
+	"What is the romaji for the hiragana 'ね'?",
+	"What is the romaji for the hiragana 'の'?",
+	"What is the romaji for the hiragana 'ぬ'?"
+]
+
+var answersArr = [
+	"だ",
+	"で",
+	"ど",
+	"ぢ",
+	"づ",
+	"da",
+	"de",
+	"do",
+	"ji",
+	"zu",
+	"な",
+	"に",
+	"ね",
+	"の",
+	"ぬ",
+	"na",
+	"ni",
+	"ne",
+	"no",
+	"nu"
+]
+
+var max_score = 20;
